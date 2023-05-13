3f564f440cf3ba9f1422e52828573263d68d628f
Biven1 <cibinnikita@gmail.com>
2023-05-13 23:28:54 +0500

    update model

--- src/app/internal/presentation/admin/menu_admin.py
+++ src/app/internal/presentation/admin/menu_admin.py
@@ -6,9 +6,20 @@ from app.internal.db.models.menu import Menu, Recommendation
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "description"]
+    pass
 
 
 @admin.register(Recommendation)
 class RecommendationAdmin(admin.ModelAdmin):
-    list_display = ["menu", "day", "breakfast", "lunch", "dinner", "kalo_sum"]
+    list_display = ["menu", "day", "get_breakfast", "get_lunch", "get_dinner", "kalo_sum"]
+    filter_horizontal = ("breakfast", "lunch", "dinner")
     readonly_fields = ("kalo_sum",)
+
+    def get_breakfast(self, obj):
+        return ",\n".join([p.name for p in obj.breakfast.all()])
+
+    def get_lunch(self, obj):
+        return ",\n".join([p.name for p in obj.lunch.all()])
+
+    def get_dinner(self, obj):
+        return ",\n".join([p.name for p in obj.dinner.all()])
