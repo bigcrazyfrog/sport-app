c6f3a33a77cb6ec30dae73125a66fb60eea8571d
Biven1 <cibinnikita@gmail.com>
2023-06-05 23:53:14 +0500

    update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,13 +39,16 @@ HOST = env('HOST')
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    "app",
+
+    # django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app",
 ]
 
 MIDDLEWARE = [
@@ -84,8 +87,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_NAME'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
 
